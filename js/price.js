1be3465fd3924bc988e4e40b58fcab667a7637c6
Brahim <brahimcasas@hotmail.es>
2021-03-19 18:15:50 +0100

    switch price

--- js/price.js
+++ js/price.js
@@ -3,13 +3,28 @@
 changing prices
 ==========================================
 */
-const colorThumbails = document
-  .querySelectorAll(".color-thumbnails-wrapper img");
+const colorThumbails = document.querySelectorAll(
+  ".color-thumbnails-wrapper img"
+);
 
-  colorThumbails.forEach(element => {
-    element.addEventListener("click", fnColorThumbails)    
-  });
+colorThumbails.forEach((element) => {
+  element.addEventListener("click", fnColorThumbails);
+});
 
 function fnColorThumbails(event) {
-    
+  // get DOM elements
+  const $price = document.querySelector(".price-wrapper h3");
+  // switch price
+  switch (event.target.getAttribute("alt")) {
+    case "Red":
+      $price.textContent = "15,55€";
+      break;
+
+    case "Blue":
+      $price.textContent = "16,55€";
+      break;
+
+    default:
+      break;
+  }
 }
