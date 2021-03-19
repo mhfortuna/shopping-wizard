c47db0f797574859ef27a57aa00695a2a8487cba
Brahim <brahimcasas@hotmail.es>
2021-03-19 17:51:56 +0100

    thumbnails Listeners

---
+++ js/price.js
@@ -0,0 +1,15 @@
+/*  
+==========================================
+changing prices
+==========================================
+*/
+const colorThumbails = document
+  .querySelectorAll(".color-thumbnails-wrapper img");
+
+  colorThumbails.forEach(element => {
+    element.addEventListener("click", fnColorThumbails)    
+  });
+
+function fnColorThumbails(event) {
+    
+}
