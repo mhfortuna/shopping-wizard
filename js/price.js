409073b2109dd23e3a9a37b112e9f14fab08ef82
Brahim <brahimcasas@hotmail.es>
2021-03-19 19:06:32 +0100

    price recuperado del stash

--- js/price.js
+++ js/price.js
@@ -14,17 +14,24 @@ colorThumbails.forEach((element) => {
 function fnColorThumbails(event) {
   // get DOM elements
   const $price = document.querySelector(".price-wrapper h3");
+  // clear previous selection
+  colorThumbails.forEach((element) => {
+    element.style.border = "none";
+  });
   // switch price
   switch (event.target.getAttribute("alt")) {
     case "Red":
-      $price.textContent = "15,55€";
+      $price.textContent = "15.55€";
       break;
 
     case "Blue":
-      $price.textContent = "16,55€";
+      $price.textContent = "16.55€";
+
       break;
 
     default:
       break;
   }
+  // Set border color
+  event.target.style.border = "2px solid red";
 }
