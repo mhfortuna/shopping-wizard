9c4b822ded94639db332424cff426480297a4da1
labietelabiete <victorhugogote@gmail.com>
2021-03-22 11:42:35 +0100

    Second commit form validation

--- js/form-validation-test.js
+++ js/form-validation-test.js
@@ -3,10 +3,10 @@ let inputFormValidation = document.querySelectorAll('.input-form');
 let pFormValidation = document.querySelectorAll('.text-form-error')
 
 function formValidation(){
-    for (let i = 0; i < labelFormValidation.length-1; i++) {
+    for (let i = 0; i < labelFormValidation.length; i++) {
         labelFormValidation[i].classList.toggle("label-form-error");
     }
-    for (let i = 0; i < inputFormValidation.length-1; i++) {
+    for (let i = 0; i < inputFormValidation.length; i++) {
         inputFormValidation[i].classList.toggle("input-form-error");
     }
     for (let i = 0; i < pFormValidation.length; i++) {
