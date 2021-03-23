9c3f08a6ebf90530613c3a08e5f33f390e6c0122
Brahim <brahimcasas@hotmail.es>
2021-03-23 09:08:08 +0100

    shipping date implementation

--- js/shipping.js
+++ js/shipping.js
@@ -26,22 +26,42 @@ checkbox.addEventListener("change", function () {
  * @ author:
  */
 function shippingDates() {
-    const obj = {};
-    // First date
-    const date = new Date();
-    date.setDate(date.getDate() + 1);
-    obj.firstDate = date.toLocaleDateString();
-    // second date
-    date.setDate(date.getDate() + 2);
-    obj.secondDate = date.toLocaleDateString();
-    return obj;
-  }
-  
-  // console.log(shippingDates());
-  /*
-   * This functions calculates shipping date
-   * and returns an object:
-   * First date: today + 24H
-   * Second date: First date + 24H
-   * @ author:
-   */
+  const obj = {};
+  // First date
+  const date = new Date();
+  date.setDate(date.getDate() + 1);
+  obj.firstDate = date.toLocaleDateString();
+  // second date
+  date.setDate(date.getDate() + 1);
+  obj.secondDate = date.toLocaleDateString();
+  // third date
+  date.setDate(date.getDate() + 1);
+  obj.thirdDate = date.toLocaleDateString();
+  return obj;
+}
+
+/*
+ * Here we update the div template
+ * to displsay the shipping date
+ * based on the shippement type
+ * Premium: today + 24H
+ * Extra: Premium + 24H
+ * @ author:
+ */
+const temp = document.querySelector(".shiptemplate");
+const bDate = temp.querySelector("b");
+const radios = document.querySelectorAll('input[type=radio][name="type"]');
+radios.forEach((radio) =>
+  radio.addEventListener("change", (event) => {
+    console.log(event.target.id);
+    if (event.target.id === "extra") {
+      bDate.textContent = shippingDates().secondDate;
+    } else if (event.target.id === "premium") {
+      bDate.textContent = shippingDates().firstDate;
+    } else {
+      bDate.textContent = shippingDates().thirdDate;
+    }
+    // show
+    temp.classList.add("shipping-show");
+  })
+);
