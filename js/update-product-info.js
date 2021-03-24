f847c5f78c8e11a427199b74063d9c6449bfd095
labietelabiete <victorhugogote@gmail.com>
2021-03-24 13:23:53 +0100

    Product pictures first commit

--- js/update-product-info.js
+++ js/update-product-info.js
@@ -1,14 +1,59 @@
+const bigPict = document.getElementsByClassName('big-picture')[0].lastElementChild;
+/*Get thumbnails*/
+const thumbs = document.querySelector('.thumbnail-wrapper').getElementsByClassName('img-thumbnail');
+const colorThumbails = document.querySelectorAll(".color-thumbnails-wrapper img");
+
+/* Add event listeners to thumbnails */
+for (let thumb of thumbs) {
+    thumb.addEventListener('click', changeProductPicture);
+    for (let i = 0; i < colorThumbails.length; i++) {
+      colorThumbails[i].addEventListener('click', changeProductPicture);
+    }
+}
+
+/* Changing product picture */
+function changeProductPicture (event) {
+    bigPict.src = event.target.src;
+    switch (event.target.dataset.color) {
+      case "Black":
+        thumbs[0].src="./images/black-01.png";
+        thumbs[1].src="./images/black-02.png";
+        thumbs[2].src="./images/black-03.png";
+        thumbs[3].src="./images/black-04.png";
+        break;
+      case "Red":
+        thumbs[0].src="./images/red-01.png";
+        thumbs[1].src="./images/red-02.png";
+        thumbs[2].src="./images/red-03.png";
+        thumbs[3].src="./images/red-04.png";
+        break;
+      case "Green":
+        thumbs[0].src="./images/green-01.png";
+        thumbs[1].src="./images/green-02.png";
+        thumbs[2].src="./images/green-03.png";
+        thumbs[3].src="./images/green-04.png";
+        break;
+      case "Yellow":
+        thumbs[0].src="./images/yellow-01.png";
+        thumbs[1].src="./images/yellow-02.png";
+        thumbs[2].src="./images/yellow-03.png";
+        thumbs[3].src="./images/yellow-04.png";
+        break;
+      case "Blue":
+        thumbs[0].src="./images/Blue-01.png";
+        thumbs[1].src="./images/Blue-02.png";
+        thumbs[2].src="./images/Blue-03.png";
+        thumbs[3].src="./images/Blue-04.png";
+        break;
+          }
+  }
+
 /*
  * This adds event listeners to DOM elements
  * waits for all elements loaded
  * @ author:
  */
 document.addEventListener("DOMContentLoaded", (event) => {
-  // get thumbnails images from DOM
-  const colorThumbails = document.querySelectorAll(
-    ".color-thumbnails-wrapper img"
-  );
-  // add listeners to all thumbnails images from DOM
   colorThumbails.forEach((element) => {
     element.addEventListener("click", fnColorThumbails);
   });
@@ -68,13 +113,22 @@ function fnSizePrice(event) {
 function calculatePriceColor(color = "Blue") {
   // color pricing
   switch (color) {
+    case "Black":
+      objPrice.color = 59.95;
+      break;
     case "Red":
-      objPrice.color = 16.55;
+      objPrice.color = 65.95;
+      break;
+    case "Green":
+      objPrice.color = 75.95;
+      break;
+    case "Yellow":
+      objPrice.color = 69.95;
       break;
     case "Blue":
-      objPrice.color = 14.55;
+      objPrice.color = 64.95;
       break;
-    default:
+        default:
       break;
   }
 }
@@ -88,19 +142,25 @@ function calculatePriceSize(size = 38) {
   // size pricing
   switch (size) {
     case "38":
+      objPrice.size = 0;
+      break;
     case "39":
-      objPrice.size = 2;
-      debugger;
+      objPrice.size = 0;
       break;
     case "40":
+      objPrice.size = 2;
+      break;
     case "41":
+      objPrice.size = 2;
+      break;
     case "42":
-      objPrice.size = 3;
+      objPrice.size = 5;
       break;
     case "43":
-    case "44":
       objPrice.size = 5;
-      debugger;
+      break;
+    case "44":
+      objPrice.size = 10;
       break;
     case "45":
       objPrice.size = 10;
@@ -123,6 +183,11 @@ function getFinalPrice() {
  * @ author:
  */
 let objPrice = {
-  color: 14.55,
+  color: 59.95,
   size: 2.0,
 };
+
+/* Page load */
+window.addEventListener("load", () => {
+  document.querySelector("body").classList.add("loaded"); 
+ });
