836e60fe99bd312f9300e792640f9650e7bfb683
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-03-24 18:51:15 +0100

    Added second date in shipping page

--- js/shipping.js
+++ js/shipping.js
@@ -39,6 +39,9 @@ function shippingDates() {
   // third date
   date.setDate(date.getDate() + 1);
   obj.thirdDate = date.toLocaleDateString();
+  // fourth date
+  date.setDate(date.getDate() + 1);
+  obj.fourthDate = date.toLocaleDateString();
   return obj;
 }
 
@@ -50,25 +53,33 @@ function shippingDates() {
  * Extra: Premium + 24H
  * @ author:
  */
-const temp = document.querySelector(".shiptemplate");
-const bDate = temp.querySelector("b");
+/* 
+const bDate = temp.querySelector("b"); */
+const shipTemplate = document.querySelector(".shiptemplate");
+const bDate1 = document.getElementById("date1");
+const bDate2 = document.getElementById("date2");
+
 const radios = document.querySelectorAll('input[type=radio][name="type"]');
 radios.forEach((radio) =>
   radio.addEventListener("change", (event) => {
     console.log(event.target.id);
     if (event.target.id === "extra") {
-      bDate.textContent = shippingDates().secondDate;
+      bDate1.textContent = shippingDates().secondDate;
+      bDate2.textContent = shippingDates().thirdDate;
       myOrder.shippingPrice = 4.99;
     } else if (event.target.id === "premium") {
-      bDate.textContent = shippingDates().firstDate;
+      bDate1.textContent = shippingDates().firstDate;
+      bDate2.textContent = shippingDates().secondDate;
       myOrder.shippingPrice = 9.99;
     } else {
       // Free
-      bDate.textContent = shippingDates().thirdDate;
+      bDate1.textContent = shippingDates().thirdDate;
+      bDate2.textContent = shippingDates().fourthDate;
       myOrder.shippingPrice = 0;
     }
     // show
-    temp.classList.add("shipping-show");
-    myOrder.shipingDate = bDate.textContent;
+    shipTemplate.classList.add("shipping-show");
+    myOrder.shippingDate1 = bDate1.textContent;
+    myOrder.shippingDate2 = bDate2.textContent;
   })
 );
